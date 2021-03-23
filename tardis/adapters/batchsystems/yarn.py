cfc01a7a3b2e58e7e88653dd8d119a724e27d620
Vincenzo Eduardo Padulano <v.e.padulano@gmail.com>
2021-03-23 16:59:52 +0100

    Define utilisation through containersCPUUSage

--- tardis/adapters/batchsystems/yarn.py
+++ tardis/adapters/batchsystems/yarn.py
@@ -10,7 +10,7 @@ import json
 import sqlite3
 
 
-logger = logging.getLogger("cobald.runtime.tardis.adapters.batchsystem.yarn")
+logger = logging.getLogger('cobald.runtime.tardis.adapters.batchsystem.yarn')
 
 
 class YarnResourceManager:
@@ -31,20 +31,45 @@ class YarnResourceManager:
     def nodes(self):
         return self._get_nodes()
 
-    def get_allocated_resources(self, node):
+    def get_node_dict(self, node):
         r = requests.get(self.base + '/ws/v1/cluster/nodes/' + node)
-        data = r.json()
-        resources = data['node']['totalResource']
-        return {'cores': resources['vCores'], 'memory': resources['memory']}
 
-    def get_used_vcores(self, node):
-        r = requests.get(self.base + '/ws/v1/cluster/nodes/' + node)
         data = r.json()
-        return data['node']['usedVirtualCores']
+        return data['node']
+
+    def get_node_resources(self, node, nm_db):
+        data = self.get_node_dict(node)
+
+        totalresources = data['totalResource']
+        resourceutilization = data['resourceUtilization']
+
+        cpus_query = 'SELECT cpus FROM yarn_nm WHERE name = ?'
+        nmconn = sqlite3.connect(nm_db)
+        with nmconn:
+            nmcursor = nmconn.cursor()
+            # Retrieve cpus from current nodemanager entry in the database
+            cpus_query_result = nmcursor.execute(
+                cpus_query, (node, )).fetchall()
+
+            # Adjust containersCPUUsage value from the YARN REST API. Normalize
+            # it to the number of cpus actually in use by YARN on this node
+            nm_cpus = cpus_query_result[0][0]
+            containers_cpu_usage = resourceutilization['containersCPUUsage'] * \
+                nm_cpus / totalresources['vCores']
+
+        nmconn.close()
+        return {
+            'id': node,
+            'cores': totalresources['vCores'],
+            'memory': totalresources['memory'],
+            'nodeCPUUsage': round(resourceutilization['nodeCPUUsage'], 2),
+            'containersCPUUsage': round(containers_cpu_usage, 2), }
 
     def set_allocated_resources(self, node, cores, memory):
-        payload = {"resource": {"memory": memory, "vCores": cores}, "overCommitTimeout": -1}
-        r = requests.post(self.base + '/ws/v1/cluster/nodes/' + node + '/resource', json=payload)
+        payload = {'resource': {'memory': memory,
+                                'vCores': cores}, 'overCommitTimeout': -1}
+        requests.post(self.base + '/ws/v1/cluster/nodes/'
+                      + node + '/resource', json=payload)
 
 
 class YarnAdapter(BatchSystemAdapter):
@@ -57,8 +82,10 @@ class YarnAdapter(BatchSystemAdapter):
     def __init__(self):
         logger.debug('Initialize Yarn BatchSystemAdapter')
         self.config = Configuration()
-        logger.debug(f'Register Yarn resourcemanager on hostname {self.config.BatchSystem.resourcemanager}')
-        logger.debug(f'Register Yarn drones database at {self.config.BatchSystem.drones_database}')
+        logger.debug(
+            f'Register Yarn resourcemanager on hostname {self.config.BatchSystem.resourcemanager}')
+        logger.debug(
+            f'Register Yarn drones database at {self.config.BatchSystem.drones_database}')
         self.rm = YarnResourceManager(self.config.BatchSystem.resourcemanager)
 
     async def disintegrate_machine(self, drone_uuid: str) -> None:
@@ -109,7 +136,34 @@ class YarnAdapter(BatchSystemAdapter):
         Get the allocation of the YARN NodeManager, which is defined as maximum
         of the ratios of requested over total resources (CPU or Memory).
         """
-        return await self.get_utilisation(drone_uuid)
+        # Find nodemanager responsible for this drone
+        sqlconn = sqlite3.connect(self.config.BatchSystem.drones_database)
+        with sqlconn:
+            cursor = sqlconn.cursor()
+            status_query = "SELECT nm FROM yarn_drones WHERE drone_uuid = ?"
+            results = cursor.execute(status_query, (drone_uuid, )).fetchall()
+        try:
+            # One of the possible machine status strings
+            drone_nm = results[0][0]
+        except:
+            raise Exception(
+                f'Failed to get nodemanager for drone {drone_uuid} from database')
+        sqlconn.close()
+
+        rm_nodes = self.rm.nodes
+        if not drone_nm in rm_nodes:
+            raise Exception(
+                f'Nodemanager of the drone in the database {drone_nm} is not in the list of available Yarn nodemanagers {rm_nodes}')
+
+        # Get allocation of this nodemanager
+        drone_nm_dict = self.rm.get_node_dict(drone_nm)
+        total_vcores = drone_nm_dict['totalResource']['vCores']
+        used_vcores = drone_nm_dict['usedVirtualCores']
+        allocation = used_vcores / total_vcores
+
+        logger.debug(
+            f'Get allocation for nodemanager {drone_nm} of drone {drone_uuid}: {allocation}')
+        return allocation
 
     async def get_machine_status(self, drone_uuid: str) -> MachineStatus:
         """
@@ -131,9 +185,11 @@ class YarnAdapter(BatchSystemAdapter):
             results = cursor.execute(status_query, (drone_uuid, )).fetchall()
         # results is a list of tuples, we're interested in the first element of the first tuple
         try:
-            drone_status = results[0][0] # One of the possible machine status strings
+            # One of the possible machine status strings
+            drone_status = results[0][0]
         except:
-            logger.debug(f'Failed to fetch status of drone {drone_uuid}, set to Draining')
+            logger.debug(
+                f'Failed to fetch status of drone {drone_uuid}, set to Draining')
             drone_status = 'Draining'
         logger.debug(f'Status is {drone_status}')
 
@@ -159,21 +215,25 @@ class YarnAdapter(BatchSystemAdapter):
             status_query = "SELECT nm FROM yarn_drones WHERE drone_uuid = ?"
             results = cursor.execute(status_query, (drone_uuid, )).fetchall()
         try:
-            drone_nm = results[0][0] # One of the possible machine status strings
+            # One of the possible machine status strings
+            drone_nm = results[0][0]
         except:
-            raise Exception(f'Failed to get nodemanager for drone {drone_uuid} from database')
+            raise Exception(
+                f'Failed to get nodemanager for drone {drone_uuid} from database')
         sqlconn.close()
 
         rm_nodes = self.rm.nodes
         if not drone_nm in rm_nodes:
-            raise Exception(f'Nodemanager of the drone in the database {drone_nm} is not in the list of available Yarn nodemanagers {rm_nodes}')
+            raise Exception(
+                f'Nodemanager of the drone in the database {drone_nm} is not in the list of available Yarn nodemanagers {rm_nodes}')
 
-        # Get utilisation of this nodemanager
-        allocated_resources = self.rm.get_allocated_resources(drone_nm)
-        allocated_vcores = allocated_resources['cores']
-        used_vcores = self.rm.get_used_vcores(drone_nm)
-        utilisation = used_vcores / allocated_vcores
-        logger.debug(f'Get utilisation for nodemanager {drone_nm} of drone {drone_uuid}: {utilisation}')
+        # Get cpu usage of this nodemanager
+        nodemanager_db = self.config.BatchSystem.nodemanager_database
+        nm_metrics = self.rm.get_node_resources(drone_nm, nodemanager_db)
+        utilisation = float(nm_metrics['containersCPUUsage']) / float(nm_metrics['cores'])
+
+        logger.debug(
+            f'Get utilisation for nodemanager {drone_nm} of drone {drone_uuid}: {utilisation}')
         return utilisation
 
     @property
